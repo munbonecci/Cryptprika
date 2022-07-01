f069272536ce3ce89e24e8e8fb77e9a9198f41ed
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2022-07-01 16:43:46 -0500

    Navigation to favorites test added

--- app/src/androidTest/java/com/munbonecci/cryptprika/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/munbonecci/cryptprika/ExampleInstrumentedTest.kt
@@ -1,10 +1,18 @@
 package com.munbonecci.cryptprika
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
+import com.munbonecci.cryptprika.core.MainActivity
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -13,6 +21,23 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun navigationToFavoritesTest() {
+        val bottomNavigationItemView = Espresso.onView(
+            allOf(
+                withId(R.id.navigation_favorites),
+                withContentDescription("Favorites"),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(ViewActions.click())
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
